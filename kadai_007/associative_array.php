60abc7c133bfa1db818f7c3986a729c22f86b1b8
yasu6kayo <3255kynyan@gmail.com>
2026-06-30 12:03:12 +0900

    課題を提出

---
+++ kadai_007/associative_array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>課題007</title>
+</head>
+<body>
+  <p>
+    <?php
+    $data = ['name' => 'orion', 'price' => 200, 'weigth' => 160];
+    print_r($data);
+    ?>
+  </p>
+</body>
+</html>
